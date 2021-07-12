e0c64b0d9714aab9f91ef6dcac79141cf26b9e6d
Christian Godfirnon <crgodfirnon@gmail.com>
2021-07-11 21:45:46 -0700

    Connection app bar with navigation

--- app/src/main/java/com/example/stock/ui/MainActivity.kt
+++ app/src/main/java/com/example/stock/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.stock.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.stock.R
 
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/example/stock/ui/StockSymbolFragment.kt
+++ app/src/main/java/com/example/stock/ui/StockSymbolFragment.kt
@@ -15,7 +15,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
